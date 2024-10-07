a18c9f584601de81fb37dd8c12a808cd21e68662
ryan-zayne <phoenixastro97@gmail.com>
2024-10-07 18:10:27 +0100

    feat: add Prettify type helper to defineEnum function
    - Extends the `defineEnum` function to use the `Prettify` type helper in addition to `Writeable`
    - This ensures the returned enum type is more readable and user-friendly

--- src/type-helpers/common.ts
+++ src/type-helpers/common.ts
@@ -1,3 +1,3 @@
-import type { Writeable } from "./types";
+import type { Prettify, Writeable } from "./types";
 
-export const defineEnum = <const TValue>(value: TValue) => value as Writeable<TValue, "deep">;
+export const defineEnum = <const TValue>(value: TValue) => value as Prettify<Writeable<TValue, "deep">>;
