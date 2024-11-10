c62a0348a2ca87cc878c28ff2e69dab5ed544da9
Ryan-Zayne <phoenixastro97@gmail.com>
2024-11-10 13:53:46 +0100

    refactor(eslint): simplify ESLint configuration and update TypeScript paths

--- eslint.config.js
+++ eslint.config.js
@@ -2,16 +2,10 @@ import { zayne } from "@zayne-labs/eslint-config";
 
 export default zayne({
 	ignores: ["packages/toolkit/dist/**"],
-	node: {
-		overrides: {
-			"node/no-unsupported-features/es-syntax": "off",
-			"node/no-unsupported-features/node-builtins": "off",
-		},
-	},
 	react: true,
 	tailwindcss: true,
 	type: "lib",
 	typescript: {
-		tsconfigPath: ["dev/tsconfig.json", "packages/toolkit/tsconfig.json"],
+		tsconfigPath: ["**/tsconfig.json"],
 	},
 });
