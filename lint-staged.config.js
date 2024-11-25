ce0f754c3a06926bb268b8414e1d40919f6a8f6d
Ryan-Zayne <phoenixastro97@gmail.com>
2024-11-25 13:30:30 +0100

    chore(lint-staged): add size lint script and increase core size limit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,4 +2,5 @@ export default {
 	"*.{js,ts,jsx,tsx}": () => "pnpm lint:eslint:dev",
 	"*.{ts,tsx}": () => "pnpm lint:check-types",
 	"package.json": ["pnpm lint:publint"],
+	"packages/**/*.{js,ts,jsx,tsx}": () => "pnpm lint:size",
 };
