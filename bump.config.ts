9146ad855d7c718a5d4990a3b6d6eccee229cd32
Ryan-Zayne <phoenixastro97@gmail.com>
2024-12-28 00:53:08 +0100

    .

--- bump.config.ts
+++ bump.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "bumpp";
 // import { globSync } from "tinyglobby";
 
 export default defineConfig({
-	// all: true,
+	all: true,
 	commit: "chore(bumpp): update package version to v",
 	files: ["./packages/**/package.json"],
-	// push: false,
+	push: false,
 	// tag: "@zayne-labs/toolkit@%",
 });
