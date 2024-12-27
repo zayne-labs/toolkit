ff3ae707a9a1213eb47b29780edca1f767ff8541
Ryan-Zayne <phoenixastro97@gmail.com>
2024-12-27 22:51:39 +0100

    chore(config): 🔧 update bump configuration and workflow

--- bump.config.ts
+++ bump.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "bumpp";
 // import { globSync } from "tinyglobby";
 
 export default defineConfig({
-	all: true,
+	// all: true,
 	// commit: "chore: update package version to v%",
 	files: ["./packages/**/package.json"],
-	push: false,
-	tag: "@zayne-labs/toolkit@%",
+	// push: false,
+	// tag: "@zayne-labs/toolkit@%",
 });
