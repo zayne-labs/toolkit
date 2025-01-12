9690089c10bd9d623dacf913525a1eeea6330200
Ryan-Zayne <phoenixastro97@gmail.com>
2025-01-12 18:38:42 +0100

    feat(toolkit-core): 🚚 Move toArray into common-utils and update exports

---
+++ packages/toolkit-core/src/common-utils.ts
@@ -0,0 +1,13 @@
+import { type AnyFunction, isArray } from "@zayne-labs/toolkit-type-helpers";
+
+export const toArray = <TValue>(value: TValue | TValue[]): TValue[] => (isArray(value) ? value : [value]);
+
+export const mergeFunctions = <TFunction extends AnyFunction<void>>(
+	...fns: Array<TFunction | undefined>
+) => {
+	const mergedFunction = (...params: Parameters<TFunction>) => fns.forEach((fn) => fn?.(...params));
+
+	return mergedFunction;
+};
+
+export const mergeClassNames = (...args: Array<string | undefined>) => args.filter(Boolean).join(" ");
--- packages/toolkit-core/src/toArray.ts
+++
@@ -1,5 +0,0 @@
-import { isArray } from "@zayne-labs/toolkit-type-helpers";
-
-const toArray = <TValue>(value: TValue | TValue[]): TValue[] => (isArray(value) ? value : [value]);
-
-export { toArray };
