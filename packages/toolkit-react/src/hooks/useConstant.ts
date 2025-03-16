60dfe02375e2705d8bdca231d55a1b5c0c3dd5a8
Ryan-Zayne <phoenixastro97@gmail.com>
2025-03-16 23:12:42 +0100

    refactor(toolkit-react): optimize useConstant hook to use useRef for better performance

--- packages/toolkit-react/src/hooks/useConstant.ts
+++ packages/toolkit-react/src/hooks/useConstant.ts
@@ -1,5 +1,13 @@
-import { useState } from "react";
+import { useRef } from "react";
 
-const useConstant = <TResult>(initCallbackFn: () => TResult) => useState(initCallbackFn)[0];
+const useConstant = <TResult>(initCallbackFn: () => TResult) => {
+	const resultRef = useRef<TResult | null>(null);
+
+	if (resultRef.current === null) {
+		resultRef.current = initCallbackFn();
+	}
+
+	return resultRef.current;
+};
 
 export { useConstant };
