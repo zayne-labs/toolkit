73ea0a1225f8514d8dc2f5a71bcf570d459037bb
Ryan-Zayne <phoenixastro97@gmail.com>
2025-03-22 14:37:12 +0100

    chore(deps): update dependencies and improve package configurations

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,4 @@
-import { baseConfig } from "@zayne-labs/eslint-config/prettier";
+import { baseConfig } from "@zayne-labs/prettier-config";
 
 const config = {
 	...baseConfig,
--- packages/toolkit-type-helpers/src/common.ts
+++ packages/toolkit-type-helpers/src/common.ts
@@ -3,5 +3,7 @@ import type { Prettify, Writeable, WriteableVariantUnion } from "./type-utils";
 export const defineEnum = <const TValue, TVariant extends WriteableVariantUnion = "shallow">(
 	value: TValue
 ) => {
-	return value as Prettify<Writeable<TValue, TVariant>>;
+	type Enum<$TValue> = Prettify<Writeable<$TValue, TVariant>>;
+
+	return value as Enum<TValue>;
 };
