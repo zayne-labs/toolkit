7cc5c9f853f4f294459a01777cc7748a13b12f15
Ryan-Zayne <phoenixastro97@gmail.com>
2025-03-29 04:22:28 +0100

    refactor(defineEnum)

--- packages/toolkit-type-helpers/src/common.ts
+++ packages/toolkit-type-helpers/src/common.ts
@@ -1,9 +1,6 @@
 import type { Prettify, Writeable, WriteableVariantUnion } from "./type-utils";
 
-export const defineEnum = <
-	TVariant extends WriteableVariantUnion = "shallow",
-	const TValue = NonNullable<unknown>,
->(
+export const defineEnum = <const TValue, TVariant extends WriteableVariantUnion = "deep">(
 	value: TValue
 ) => {
 	return value as Prettify<Writeable<TValue, TVariant>>;
