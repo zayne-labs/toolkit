df886f27926bec2672ceb03576cea12b9863a271
Ryan-Zayne <phoenixastro97@gmail.com>
2025-04-16 23:30:01 +0100

    refactor: types

--- packages/toolkit-react/src/utils/composeEventHandlers.ts
+++ packages/toolkit-react/src/utils/composeEventHandlers.ts
@@ -1,10 +1,12 @@
-type PossibleEventHandler<TEvent extends React.SyntheticEvent> = React.EventHandler<TEvent> | undefined;
+type PossibleEventHandler<TSyntheticEvent extends React.SyntheticEvent> =
+	| React.EventHandler<TSyntheticEvent>
+	| undefined;
 
-export const composeEventHandlers = <TEvent extends React.SyntheticEvent>(
-	eventHandlers: Array<PossibleEventHandler<TEvent>>
-): React.EventHandler<TEvent> => {
+export const composeEventHandlers = <TSyntheticEvent extends React.SyntheticEvent>(
+	eventHandlers: Array<PossibleEventHandler<TSyntheticEvent>>
+): React.EventHandler<TSyntheticEvent> => {
 	// prettier-ignore
-	const mergedEventHandler: React.EventHandler<TEvent> = (event) => eventHandlers.forEach((handler) => handler?.(event));
+	const mergedEventHandler: React.EventHandler<TSyntheticEvent> = (event) => eventHandlers.forEach((handler) => handler?.(event));
 
 	return mergedEventHandler;
 };
