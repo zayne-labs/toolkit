48e686fb7b1f33df68f94e3d675ac9ca7ed7f342
Ryan-Zayne <phoenixastro97@gmail.com>
2025-05-08 00:30:28 +0100

    feat(toolkit-react)!: 🗑️ Remove deprecated getSlot and getSlotMap utilities and move it to @zayne-labs/ui

--- packages/toolkit-react/src/utils/index.ts
+++ packages/toolkit-react/src/utils/index.ts
@@ -1,7 +1,5 @@
 export * from "./composeRefs";
 export * from "./composeEventHandlers";
-export * from "./getSlot";
-export * from "./getSlotMap";
 export { mergeProps } from "./mergeProps";
 export { mergeTwoProps } from "./mergeTwoProps";
 export * from "./types";
