76e3cc293f632e2eac991d4d148b7f20e941d932
Ryan-Zayne <phoenixastro97@gmail.com>
2025-05-20 12:57:49 +0100

    feat(toolkit-core): ✨ Add dataAttr utility function for data attribute handling

--- packages/toolkit-core/src/common-utils.ts
+++ packages/toolkit-core/src/common-utils.ts
@@ -2,6 +2,8 @@ import { isArray } from "@zayne-labs/toolkit-type-helpers";
 
 export const toArray = <TValue>(value: TValue | TValue[]): TValue[] => (isArray(value) ? value : [value]);
 
+export const dataAttr = (condition: unknown) => (condition ? "true" : undefined) as unknown as boolean;
+
 // const pipe = <TValue>(functions: Array<(value: TValue) => TValue>) => {
 // 	const pipedFn = (value: TValue) =>
 // 		functions.reduce((accumulatedValueResult, fn) => fn(accumulatedValueResult), value);
