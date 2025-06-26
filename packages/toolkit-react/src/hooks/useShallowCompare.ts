0a59439a6627235d0a4fb624f7745fe45aad739e
Ryan-Zayne <phoenixastro97@gmail.com>
2025-06-26 17:38:00 +0100

    fix: forgot to export the hook

--- packages/toolkit-react/src/hooks/useShallowCompare.ts
+++ packages/toolkit-react/src/hooks/useShallowCompare.ts
@@ -1,9 +1,8 @@
 import { shallowCompare } from "@zayne-labs/toolkit-core";
+import type { SelectorFn } from "@zayne-labs/toolkit-type-helpers";
 import { useRef } from "react";
 
-const useShallowCompare = <TState, TResult>(
-	selector: (state: TState) => TResult
-): ((state: TState) => TResult) => {
+const useShallowCompare = <TState, TResult>(selector: SelectorFn<TState, TResult>) => {
 	const prev = useRef<TResult>(undefined as never);
 
 	const shallowSelector = (state: TState) => {
