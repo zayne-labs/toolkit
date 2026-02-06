8068a98320c5f7e10026d54cac652f8df24a802a
Ryan-Zayne <phoenixastro97@gmail.com>
2026-02-06 22:47:41 +0100

    chore(bumpp): update package version to v0.12.32

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 export default {
-	"*.{js,ts,jsx,tsx}": () => ["pnpm test", "pnpm lint:eslint:root"],
-	"*.{ts,tsx}": () => "pnpm lint:type-check",
+	"*.{js,ts,jsx,tsx}": () => "pnpm lint:eslint:root",
+	"*.{ts,tsx}": () => ["pnpm test", "pnpm lint:type-check"],
 	"package.json": () => "pnpm lint:publint",
 	"packages/**/*.{js,ts,jsx,tsx}": () => "pnpm lint:size",
 };
